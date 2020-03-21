64f0cf69780a36d51ae17c12dc92b371c6bc018a
Christoph Waibel <christophwaibel@gmx.de>
2020-03-21 18:38:26 +0100

    restructured repo

---
+++ Cplex_EnergyHubs/AdamMSc2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cplex_Adam2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
