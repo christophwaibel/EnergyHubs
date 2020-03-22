4b197f68776c138b39c24d4f25c3cca37c88a74b
Christoph Waibel <christophwaibel@gmx.de>
2020-03-22 22:26:28 +0100

    standard k-means should work now

--- Cplex_EnergyHubs/AdamMSc2020/Program.cs
+++ Cplex_EnergyHubs/AdamMSc2020/Program.cs
@@ -8,7 +8,20 @@ namespace AdamMSc2020
         {
             Console.WriteLine("Cplex test");
 
-      
+            Random rnd = new Random(34);
+
+            double[][] randomData = new double[100][];
+            for(int i=0; i<100; i++)
+            {
+                randomData[i] = new double[2];
+                randomData[i][0] = rnd.NextDouble();
+                randomData[i][1] = rnd.NextDouble();
+            }
+
+            Tuple<double[][], int[][]> tuple = EhubMisc.Clustering.KMeans(randomData, 3);
+
+          
+            Console.ReadKey();
 
 
         }
--- Cplex_EnergyHubs/EhubMisc/Misc.cs
+++ Cplex_EnergyHubs/EhubMisc/Misc.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace EhubMisc
 {
@@ -9,6 +10,20 @@ namespace EhubMisc
         public static bool IsNullOrDefault<T>(this Nullable<T> value) where T : struct
         {
             return default(T).Equals(value.GetValueOrDefault());
+        }
+
+
+        public static double Distance2Pts(double[] x1, double[] x2)
+        {
+            int n = x1.Length;
+            if (x2.Length < n) n = x2.Length;
+
+            double distance = 0.0;
+            for (int i = 0; i < n; i++)
+                distance += Math.Pow(x1[i] - x2[i], 2);
+            distance = Math.Sqrt(distance);
+
+            return distance;
         }
     }
 
@@ -59,43 +74,90 @@ namespace EhubMisc
 
     public static class Clustering
     {
-
         /// <summary>
         /// K-Means clustering
         /// </summary>
         /// <param name="dataset"></param>
-        /// <param name="nClusters"></param>
+        /// <param name="clusters"></param>
+        /// <param name="iterations">50 is enough: Scalable K-Means by Ranked Retrieval. Broder et al. 2014, WSDM'14; http://dx.doi.org/10.1145/2556195.2556260 </param>
+        /// <param name="seed"></param>
         /// <returns></returns>
-        public static Tuple<double [,], int [][]> KMeans(double [,] dataset, int Clusters)
+        public static Tuple<double[][], int[][]> KMeans(double[][] dataset, int clusters, int iterations = 50, int seed = 34)
         {
-            int n = dataset.GetLength(1); // problem dimension
-            int m = dataset.GetLength(0); // number of dataset
-            int K = Clusters;
+            Random rnd = new Random(seed);
 
-            // Pseudo code
-            /*
-            Randomly initialize K cluster centrods, mu1, mu2, ..., mu_k \in R^n
-            Repeat {
-                // cluster assignment step
-                for i=1 to m
-                    c^(i) := index(from 1 to K) of clister centroid closest to x^(i)
-                            -> c^(i) = min _k ||x^(i) - mu_k||^2
-                
-                // move centroid
-                for k = 1 to K
-                    mu_k := mean of points assigned to cluster k
+            int n = dataset[0].Length; // problem dimension
+            int m = dataset.Length; // size of dataset
+            int K = clusters;
+
+            //// Pseudo code, from Andrew Ng's Machine Learning lecture on coursera
+            /// could be improved, e.g. https://arxiv.org/pdf/1801.02949.pdf
+            //Randomly initialize K cluster centrods, mu1, mu2, ..., mu_k \in R^n
+            //Repeat {
+            //    // cluster assignment step
+            //    for i=1 to m
+            //        c^(i) := index(from 1 to K) of clister centroid closest to x^(i)
+            //                -> c^(i) = min _k ||x^(i) - mu_k||^2
+
+            //    // move centroid
+            //    for k = 1 to K
+            //        mu_k := mean of points assigned to cluster k
+            //}
+
+            // initialize random centroids
+            List<int> alreadySelected = new List<int>();
+            double[][] means = new double[K][];
+            for (int _k = 0; _k < K; _k++)
+            {
+                int selectedIndex = rnd.Next(0, m);
+                if (_k > 0)
+                {
+                    while (alreadySelected.IndexOf(selectedIndex) != -1)
+                        selectedIndex = rnd.Next(0, m);
+                    alreadySelected.Add(selectedIndex);
+                }
+                else alreadySelected.Add(selectedIndex);
+
+                means[_k] = new double[n];
+                for (int _n = 0; _n < n; _n++)
+                    means[_k][_n] = dataset[selectedIndex][_n];
             }
-            */
 
+            // iterate
+            List<int>[] clusterItems = new List<int>[K];
+            for (int _iter = 0; _iter < iterations; _iter++)
+            {
+                // cluster assignment
+                clusterItems = new List<int>[K];
+                for (int _k = 0; _k < K; _k++)
+                    clusterItems[_k] = new List<int>();
 
+                for (int i = 0; i < m; i++)
+                {
+                    List<double> distancesToCentroids = new List<double>();
+                    foreach (double[] centroid in means)
+                        distancesToCentroids.Add(Misc.Distance2Pts(dataset[i], centroid));
 
+                    int minimumValueIndex = distancesToCentroids.IndexOf(distancesToCentroids.Min());
+                    clusterItems[minimumValueIndex].Add(i);
+                }
 
+                // move centroids
+                for (int _k = 0; _k < K; _k++)
+                {
+                    means[_k] = new double[n];
+                    foreach (int item in clusterItems[_k])
+                        for (int _n = 0; _n < n; _n++)
+                            means[_k][_n] += dataset[item][_n];
 
+                    for (int _n = 0; _n < n; _n++)
+                        means[_k][_n] /= clusterItems[_k].Count;
+                }
+            }
 
-
-
-            double[,] means = new double[K, n];
             int[][] indices = new int[K][];
+            for (int _k = 0; _k < K; _k++)
+                indices[_k] = clusterItems[_k].ToArray();
             return Tuple.Create(means, indices);
         }
 
@@ -106,7 +168,7 @@ namespace EhubMisc
         /// <param name="dataX"></param>
         /// <param name="nClusters"></param>
         /// <returns></returns>
-        public static Tuple<double [,], int [][]> KMedoids(double [,] dataX, int nClusters)
+        public static Tuple<double[,], int[][]> KMedoids(double[,] dataX, int nClusters)
         {
             int dim = dataX.GetLength(1);
 
