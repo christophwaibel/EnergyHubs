6a0ed63a88889534ffd2db9b569ee1ef276906a3
christophwaibel <christophwaibel@gmx.de>
2022-11-01 11:48:41 +0100

    creating project

---
+++ CplexEnergyHubs/SBE22BuildingIntegratedAgriculture/EHubBiA.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SBE22BuildingIntegratedAgriculture
+{
+    class EHubBiA
+    {
+    }
+}
---
+++ CplexEnergyHubs/SBE22BuildingIntegratedAgriculture/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SBE22BuildingIntegratedAgriculture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JObject o1 = JObject.Parse(File.ReadAllText(@"C:\Users\chwaibel\Desktop\BIA\outputs\data\solar-radiation\B1004_insolation_Whm2.json"));
+        }
+
+
+  
+    }
+
+
+}
