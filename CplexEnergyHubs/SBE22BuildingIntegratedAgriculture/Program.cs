456fea224eff558562c43005947e9993ec25cb5f
christophwaibel <christophwaibel@gmx.de>
2022-11-08 16:53:50 +0100

    this is geometry file

--- CplexEnergyHubs/SBE22BuildingIntegratedAgriculture/Program.cs
+++ CplexEnergyHubs/SBE22BuildingIntegratedAgriculture/Program.cs
@@ -10,16 +10,22 @@ namespace SBE22BuildingIntegratedAgriculture
     {
         static void Main(string[] args)
         {
+            // solar potentials
+            // several thousands of surfaces
             JObject o1 = JObject.Parse(File.ReadAllText(@"\\nas22\arch_ita_schlueter\03-Research\01-Projects\29_FCLGlobal\04_NumericalExperiments\BuildingIntegratedAgriculture\Zhongming Data\outputs\data\solar-radiation\B1004_insolation_Whm2.json"));
             //var stuff = o1["srf0"].ToObject<double[]>();
 
             Dictionary<string, double[]> dictObj = o1.ToObject<Dictionary<string, double[]>>();
             o1 = null;
 
+
+            // surface areas
+            // "...\B1004_geometry.csv" instead of "...\B1004_insolation_Whm2.json"
+
         }
 
 
-  
+
     }
 
 
